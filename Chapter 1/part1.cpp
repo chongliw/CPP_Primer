ab417ad062bfe80f88490f7463b666407894ecba
chongli <chongli.cn@gmail.com>
2015-09-10 11:15:07 -0400

    delete extra endl

--- Chapter 1/part1.cpp
+++ Chapter 1/part1.cpp
@@ -23,5 +23,4 @@ void test001_iostream() {
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
     std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
-    std::endl;
 }
